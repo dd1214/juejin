d1dee9d9d09aab6dd7e628a0c9acae43f0748832
xun <963183614@qq.com>
2022-07-23 21:30:58 +0800

    测试分支合并

---
+++ vue/vue-juejin/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
+createApp(App).mount('#app')
