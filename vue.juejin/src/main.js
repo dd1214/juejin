78fddcbd60ea758046657799c3d2f536a6319afb
zzq <751050931@qq.com>
2022-08-16 12:39:11 +0800

    最上方主导航栏的样式，以及修改了 elementUI 主色，添加了几个图标，添加了一些基本样式

--- vue.juejin/src/main.js
+++ vue.juejin/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import '@/css/base.css'
+import '@/css/element-variables.scss'
 
 Vue.config.productionTip = false
 
