8c4a8b7c4ee5e08d11595fcbb8c9ac18c1b7841e
zzqba <751050931@qq.com>
2022-08-19 19:54:14 +0800

    张 - 准备开始重构文章区域

--- vue.juejin/src/components/Essay.vue
+++ vue.juejin/src/components/Essay.vue
@@ -1,13 +1,39 @@
+<!-- 内容区的每一项 -->
+<!-- 数据有：发稿人、发稿时间、分类、文章标题、文章内容前 x 个字、点赞、评论、查看 -->
 <template>
-        <div class="article white shadow mgb1">{{EssayTitle}}</div>
+  <div class="container">
+    <!-- 第一行：稿件信息 -->
+    <div class="info">
+
+    </div>
+    <!-- 第二行：稿件内容预览 -->
+    <div class="content">
+
+    </div>
+    <!-- 第三行：查看、点赞、评论 -->
+    <div class="feedback">
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Essay',
-    props: ['EssayTitle'], 
-    data(){
-        return {}
-    }
+  name: 'Essay',
+  props: ['EssayTitle'],
+  data() {
+    return {}
+  },
 }
 </script>
+
+<style scoped lang="less">
+// 最外部容器，控制位置
+.container {
+  .info{
+
+    font-size: 13px;
+    color: #4e5969;
+  }
+}
+</style>
---
+++ vue.juejin/src/components/Essays.vue
@@ -0,0 +1,20 @@
+<template>
+  <Essay>
+    
+  </Essay>
+</template>
+
+<script>
+import Essay from '@/components/Essay'
+
+export default {
+  name: 'Essays',
+  components: {
+    Essay
+  },
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
