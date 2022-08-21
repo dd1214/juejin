c0f5309551b28e4df59d8b65168644bd58e14ade
BlueSheepLX <100213545+BlueSheepLX@users.noreply.github.com>
2022-08-22 05:25:51 +0800

    戴-新增views文件夹，用于存放页面

---
+++ vue.juejin/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navbar/>
+        <LowerHalf/>
+    </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Adv from "@/components/adv.vue";
+import LowerHalf from "@/components/LowerHalf.vue";
+
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: { Navbar, Adv, LowerHalf },
+}
+</script>
+
