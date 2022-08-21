3bad81ca586771ea1ba7c1a0e2e3e9e3fb0e0667
BlueSheepLX <100213545+BlueSheepLX@users.noreply.github.com>
2022-08-22 05:27:02 +0800

    修改路由配置

--- vue.juejin/src/router/index.js
+++ vue.juejin/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from "../App.vue"
-import atc from '../components/atc.vue';
+import Home from "../views/Home.vue"
+import Article from '../views/Article.vue';
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-
+  mode:'history',
   routes:[
     {//重定向
       path:'/',
@@ -14,11 +14,13 @@ const router = new VueRouter({
       component: Home,
     },
     {
-      path:'/details',
+      path:'/article',
       name:'xiangqingye',
-      component: atc,
+      component: Article,
     }
   ]
+
+
 
 })
 
