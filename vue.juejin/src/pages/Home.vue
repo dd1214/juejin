5c37ae67698d11d3602ba7f81dcab33fd2c652c0
xun <963183614@qq.com>
2022-08-23 14:44:21 +0800

    廖-注册登陆组件

--- vue.juejin/src/pages/Home.vue
+++ vue.juejin/src/pages/Home.vue
@@ -3,8 +3,13 @@
   <!-- 上半区 -->
   <Navbar/>
   <LinkPop/>
+  <!-- 注册登陆 -->
+  <!-- <Land/>
+  <Register/> -->
   <!-- 下半区 -->
   <LowerHalf/>
+  
+
 </div>
 </template>
 
@@ -12,12 +17,14 @@
 import Navbar from '@/components/Navbar.vue'
 import LowerHalf from '@/components/LowerHalf.vue'
 import LinkPop from '@/components/Linkpop.vue'
+import Land from '@/components/Land.vue'
+import Register from '@/components/Register.vue'
 
 
 export default {
   name: 'Home',
   components: {
-   Navbar,LowerHalf,LinkPop
+   Navbar,LowerHalf,LinkPop,Land,Register
   }
 }
 </script>
