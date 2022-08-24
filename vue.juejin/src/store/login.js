551de4b2fb8bde9d654fc67d8ed33c64bd958284
zzqba <751050931@qq.com>
2022-08-24 10:36:36 +0800

    尝试做登录

---
+++ vue.juejin/src/store/login.js
@@ -0,0 +1,18 @@
+export default {
+  namaspaced: true,
+  state:{
+    // 是否已经登录
+    isLogin: false,
+    // 保存用户登录状态
+    userInfo: {
+
+    }
+  },
+  actions: {
+    
+  },
+  mutations: {
+
+  },
+
+}
