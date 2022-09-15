d71a111df41bcaa703ed131a2b0c8db2825e0366
xun <963183614@qq.com>
2022-09-15 18:26:33 +0800

    广告栏增加签到

--- vue.juejin/src/components/adv.vue
+++ vue.juejin/src/components/adv.vue
@@ -2,6 +2,30 @@
   <div class="flex mgr8">
     <!-- 广告组件 -->
     <div class="flex1">
+      <div class="sign column flexCenter mgt8">
+        <div class="flex flexCenter ">
+          <svg width="60" 
+          height="30" 
+          viewBox="0 0 24 24" >
+          <path d="M8 2C8 1.72386 7.77614 1.5 7.5 1.5H6.5C6.22386 1.5 6 1.72386 6 2L5.9995 
+          3H3C2.44772 3 2 3.47259 2 4.05556V7H22V4.05556C22 3.47259 21.5523 3 21 3H18V2C18 
+          1.72386 17.7761 1.5 17.5 1.5H16.5C16.2239 1.5 16 1.72386 16 2V3H8V2ZM22 8.5H2V20
+          .9444C2 21.5274 2.44772 22 3 22H21C21.5523 22 22 21.5274 22 20.9444V8.5Z" 
+          fill="#FFCF8B" ></path>
+              <rect x="5" y="12" width="3" height="2" rx="1" fill="#FF7D00" ></rect>
+              <rect x="10.5" y="12" width="3" height="2" rx="1" fill="#FF7D00" ></rect>
+              <rect x="5" y="16" width="3" height="2" rx="1" fill="#FF7D00" >
+                </rect><rect x="10.5" y="16" width="3" height="2" rx="1" fill="#FF7D00" ></rect>
+                <rect x="16" y="12" width="3" height="2" rx="1" fill="#FF7D00" ></rect>
+                <rect x="16" y="16" width="3" height="2" rx="1" fill="#FF7D00" ></rect>
+                </svg>
+             <span style="font-size:18px;font-weight: 400">下午好！</span>
+    
+       <el-button round class="round-button">去签到</el-button>
+       </div>
+       <br>
+       <span style="font-weight: 300;font-size:15px">点亮你在社区的每一天</span>
+       </div>
       <div class="mgt8 ">
         <div class="outsideDiv" >
           <img :src="adv1" class="" v-show="item1" />
@@ -82,5 +106,17 @@ img{
 }
 i{
     cursor: pointer;
+}
+.sign{
+  background-color:white;
+  display:flex;
+  height: 100px;
+  width:240px;
+}
+.round-button{
+  height:40px;
+  border-color:#1e80ff;
+  color:#1e80ff;
+  font-size:14px;
 }
 </style>
