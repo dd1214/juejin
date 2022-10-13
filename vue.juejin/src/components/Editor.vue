966832e750ce99b84e98d34571d7d5c355576238
xun <963183614@qq.com>
2022-10-13 18:15:15 +0800

    设置编辑器，准备实时预览

--- vue.juejin/src/components/Editor.vue
+++ vue.juejin/src/components/Editor.vue
@@ -1,46 +1,85 @@
 <template>
-    <div style="border: 1px solid #ccc;">
+<div>
+    <el-button type="primary" plain class="mg8">提交文章</el-button>
+    <div class="flex">
+
+    <div style="border: 1px solid #ccc; width:50%">
         <Toolbar
             style="border-bottom: 1px solid #ccc"
             :editor="editor"
-            :defaultConfig="toolbarConfig"
             :mode="mode"
+            :defaultConfig="toolbarConfig"
         />
         <Editor
-            style="height: 500px; overflow-y: hidden;"
+            style="height: 400px; overflow-y: hidden;"
             v-model="html"
             :defaultConfig="editorConfig"
             :mode="mode"
             @onCreated="onCreated"
         />
     </div>
+    <div style="border: 1px solid #ccc; width:50%">实时预览区域</div>
+        </div>
+    </div>
 </template>
   
   <script>
 import Vue from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
-
+import { DomEditor } from '@wangeditor/editor'
 export default Vue.extend({
     components: { Editor, Toolbar },
     data() {
         return {
             editor: null,
-            html: '<p>hello</p>',
-            toolbarConfig: { },
-            editorConfig: { placeholder: '请输入内容...' },
+            html: '<p>欢迎来到编辑页</p>',
+            toolbarConfig:{toolbarKeys:[
+                    'headerSelect',//格式区
+                    "blockquote",//引用
+                    '|',
+                    "bold",//黑体
+                    "underline",//下划线
+                     'italic', //删除线
+                    "through",
+                    
+                    '|',//添加区
+                    "codeBlock",//代码块 代码高亮使用Prism.js实现
+                    "emotion",//表情
+                    "bulletedList",//无序列表
+                    "numberedList",//有序列表
+                    "insertLink",//插入链接
+                    "insertTable",//插入表格
+                    {
+                    key: 'group-add', // 要以 group 开头
+                    title: '...',
+                    menuKeys: ["todo","sub","sup","enter","divider",] // 下级菜单的key //分割线
+                    }, 
+                    '|',//文字编辑区
+                    "fontFamily",//默认字体
+                    "fontSize",  
+                    "color",
+                    "bgColor",
+                    '|',//图像区
+                    "uploadImage",
+                    "editImage",
+                    '|',//功能区
+                    "undo",
+                    "redo",
+                    "fullScreen",
+                ]},
+                
+            editorConfig: { placeholder: '请输入您的文章' },
             mode: 'default', // or 'simple'
+            toolbar:DomEditor.getToolbar(this.editor),
         }
     },
     methods: {
         onCreated(editor) {
             this.editor = Object.seal(editor) // 一定要用 Object.seal() ，否则会报错
         },
+        
     },
     mounted() {
-        // 模拟 ajax 请求，异步渲染编辑器
-        setTimeout(() => {
-            this.html = '<p>模拟 Ajax 异步设置内容 HTML</p>'
-        }, 1500)
     },
     beforeDestroy() {
         const editor = this.editor
