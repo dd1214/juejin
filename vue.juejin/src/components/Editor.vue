ab08d30c97545c8ea42464a1281c38b71b638349
xun <963183614@qq.com>
2022-10-14 11:39:55 +0800

    实现预览效果

--- vue.juejin/src/components/Editor.vue
+++ vue.juejin/src/components/Editor.vue
@@ -1,34 +1,40 @@
 <template>
 <div>
-    <el-button type="primary" plain class="mg8">提交文章</el-button>
-    <div class="flex">
+    <el-button type="primary" plain class="mg8" @click="preview">预览文章</el-button>
+    <div class="flex column">
 
-    <div style="border: 1px solid #ccc; width:50%">
+    <div style="border: 1px solid #ccc; width:100%" >
         <Toolbar
             style="border-bottom: 1px solid #ccc"
             :editor="editor"
             :mode="mode"
             :defaultConfig="toolbarConfig"
         />
-        <Editor
-            style="height: 400px; overflow-y: hidden;"
+       
+    </div> 
+
+    <div class="flex">
+    <Editor
+            style="height: 400px; overflow-y: hidden;width:50%;"
             v-model="html"
             :defaultConfig="editorConfig"
             :mode="mode"
             @onCreated="onCreated"
+            @onChange="onChange"
         />
+     <Preview :htmlData="getHTMLdata"/>   
+       </div>  
     </div>
-    <div style="border: 1px solid #ccc; width:50%">实时预览区域</div>
-        </div>
-    </div>
+ </div>
 </template>
   
   <script>
 import Vue from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 import { DomEditor } from '@wangeditor/editor'
+import Preview from "./Preview.vue"
 export default Vue.extend({
-    components: { Editor, Toolbar },
+    components: { Editor, Toolbar, Preview },
     data() {
         return {
             editor: null,
@@ -67,19 +73,36 @@ export default Vue.extend({
                     "redo",
                     "fullScreen",
                 ]},
-                
             editorConfig: { placeholder: '请输入您的文章' },
             mode: 'default', // or 'simple'
             toolbar:DomEditor.getToolbar(this.editor),
+            getHTMLdata:null,
         }
     },
     methods: {
         onCreated(editor) {
             this.editor = Object.seal(editor) // 一定要用 Object.seal() ，否则会报错
         },
+        onChange(editor) { 
+            //采用节流，输入完之后不会立即更新
+            let timer = null
+                if(timer) {
+                return 
+                }
+             timer = setTimeout(() => {
+                this.getHTMLdata=editor.getHtml() //更新预览
+                timer = null
+                    },500)
+           
+            },
+        preview(){
+                 const editor = this.editor // 获取 editor 实例
+                 this.getHTMLdata=(editor.getHtml())
+        },
         
     },
     mounted() {
+        
     },
     beforeDestroy() {
         const editor = this.editor
---
+++ vue.juejin/src/components/Preview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="preview">
+    <div v-html="htmlData"></div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name:'Preview',
+    props:{
+      htmlData:{
+        type: String,
+        default:''
+      }},
+    mounted(){
+
+    },
+    
+  
+}
+</script>
+
+<style scoped>
+.preview{
+  border: 1px solid #ccc;
+  width:50%;
+  max-height:600px;
+  overflow:auto;
+}
+</style>
